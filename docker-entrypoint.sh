260ef82cd01ae2c3fa31dedd12e419135fb7fd63
svvssowmya97 <svvssowmya97@gmail.com>
2023-04-14 07:38:20 +0530

    added docker files

---
+++ docker-entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#export MY_COMMAND_1="echo 'Hello World!'"
+#export MY_COMMAND_2="ls -l"
+
+
+usr/bin/java -jar /usr/src/app/target/testproject-0.0.1-SNAPSHAOT.jar com.example.demo.TestprojectApplication
